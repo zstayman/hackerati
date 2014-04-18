1d5bb1bc0ddb6bbca0f15d04bedd0c77c8419c45
github_name <github_email>
2014-04-18 17:49:44 -0400

    done

--- auction_lib/lib/auction.rb
+++ auction_lib/lib/auction.rb
@@ -2,14 +2,75 @@ require 'pry'
 class Auction
   @@lots = {}
 
-  def initialize(name)
-    unless @@lots[name.downcase.gsub(" ", "_").to_sym]
+  def initialize(name, reserve=0)
+    unless @@lots[name.hash_key]
       @name = name
-      @@lots[@name.downcase.gsub(" ", "_").to_sym] = self
+      @reserve = reserve
+      @@lots[self.name.hash_key] = self
+      @active = false
+      @current_bid = 0
+      @high_bidder = ""
+      @status = "#{self.name} has not yet begun."
     end
   end
 
   def self.lots
     return @@lots
+  end
+
+  def start
+    @active = true
+    @status = "The auction on #{self.name} has begun, but nobody has bid yet"
+  end
+
+  def active?
+    return @active
+  end
+
+  def bid(amount, bidder_name)
+    if self.active?
+      if amount > self.high_bid?
+        @current_bid = amount
+        @high_bidder = bidder_name
+        @status = "The highest bidder on Art is #{self.high_bidder} with $#{self.high_bid?}"
+      end
+    end
+  end
+
+  def high_bid?
+    return @current_bid
+  end
+
+  def high_bidder
+    return @high_bidder
+  end
+
+  def end
+    @active = false
+    if self.high_bid? > @reserve
+      @status = "The auction has concluded, #{self.high_bidder} won with a bid of $#{self.high_bid?}"
+    else
+      @status = "#{self.name} failed to sell because the reserve price was not met"
+    end
+  end
+
+  def status
+    return @status
+  end
+
+  def name
+    return @name
+  end
+
+  def self.query(name)
+    self.lots[name.hash_key].status
+  end
+
+
+end
+
+class String
+  def hash_key
+    self.downcase.gsub(" ", "_").to_sym
   end
 end
--- auction_lib/spec/auction_spec.rb
+++ auction_lib/spec/auction_spec.rb
@@ -1,19 +1,85 @@
 require 'spec_helper'
 require_relative '../lib/auction'
+require 'pry'
 
 describe Auction do
   before(:each) do
     Auction.class_variable_set :@@lots, {}
+    @auction = Auction.new("Art", 100)
   end
   it "should have a name" do
-    lot = Auction.new("Art")
-    expect(Auction.lots[:art]).to eq(lot)
+    expect(Auction.lots[:art]).to eq(@auction)
   end
 
   it "should have a unique name" do
-    lot = Auction.new("art")
-    Auction.new("art")
-    expect(Auction.lots[:art]).to eq(lot)
+    Auction.new("Art")
+    expect(Auction.lots[:art]).to be(@auction)
+  end
+
+  describe "Auction#start" do
+    it "should be able to be started" do
+      @auction.start
+      expect(@auction.active?).to eq(true)
+    end
+  end
+
+  describe "Auction#active?" do
+    it "should tell you if the auction has started" do
+      expect(@auction.active?).to eq(false)
+      @auction.start
+      expect(@auction.active?).to eq(true)
+    end
+  end
+
+  describe "Auction#bid" do
+    it "changes the bid unless there is already a higher bid" do
+      @auction.start
+      @auction.bid(2000, "Zack")
+      expect(@auction.high_bid?).to eq(2000)
+      expect(@auction.high_bidder).to eq("Zack")
+      @auction.bid(1000, "Joe")
+      expect(@auction.high_bid?).to eq(2000)
+      expect(@auction.high_bidder).to eq("Zack")
+    end
+
+    describe "Auction#end" do
+      it "ends the auction" do
+        @auction.end
+        expect(@auction.active?).to eq(false)
+      end
+    end
+
+    describe "Auction#status" do
+      context "a successful auction" do
+        it "describes the status of the auction" do
+          expect(@auction.status).to eq("Art has not yet begun.")
+          @auction.start
+          expect(@auction.status).to eq("The auction on Art has begun, but nobody has bid yet")
+          @auction.bid(1000, "Zack")
+          expect(@auction.status).to eq("The highest bidder on Art is Zack with $1000")
+          @auction.bid(2000, "Joe")
+          expect(@auction.status).to eq("The highest bidder on Art is Joe with $2000")
+          @auction.end
+          expect(@auction.status).to eq("The auction has concluded, Joe won with a bid of $2000")
+        end
+      end
+
+      context "an auction where the high bid does not meet the reserve" do
+        it "describes the status of the auction" do
+          @auction.start
+          @auction.bid(10, "Jim")
+          @auction.end
+          expect(@auction.status).to eq("Art failed to sell because the reserve price was not met")
+        end
+      end
+
+      describe "query" do
+        it "Allows you to search for the status of a specific item" do
+          expect(Auction.query("Art")).to eq("Art has not yet begun.")
+        end
+      end
+
+    end
   end
 
 end
